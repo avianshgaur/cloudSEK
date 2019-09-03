abdedae7d1654c4a0e36cc91585a7ff515fba0bf
avianshgaur <45706789+avianshgaur@users.noreply.github.com>
2019-09-03 21:40:45 +0530

    Update app.py

--- 1/app.py
+++ 1/app.py
@@ -4,64 +4,28 @@ import requests
 import os
 from time import time
 from multiprocessing.pool import ThreadPool
-
-##url = 'https://www.python.org/static/img/python-logo@2x.png'
-
-##urls = [("Event1", "https://www.python.org/events/python-events/805/"),
-##
-##("Event2", "https://www.python.org/events/python-events/801/"),
-##
-##("Event3", "https://www.python.org/events/python-events/790/"),
-##
-##("Event4", "https://www.python.org/events/python-events/798/"),
-##
-##("Event5", "https://www.python.org/events/python-events/807/"),
-##
-##("Event6", "https://www.python.org/events/python-events/807/"),
-##
-##("Event7", "https://www.python.org/events/python-events/757/"),
-##
-##("Event8", "https://www.python.org/events/python-user-group/816/")]
-##
-
-
-
-
-def is_downloadable(url):
-    """
-    Does the url contain a downloadable resource
-    """
-    h = requests.head(url, allow_redirects=True)
-    header = h.headers
-    content_type = header.get('content-type')
-    if 'text' in content_type.lower():
-        return False
-    if 'html' in content_type.lower():
-        return False
-    return True
-url = 'http://aviaryan.in/images/profile.png'
+##names the file
 def filename(url):
     if url.find('/'):
       return url.rsplit('/', 1)[1]
 
-
+#for multithread
 def url_response(url):
     path,url = url
     name = filename(url)
-    #print('c:/users/AVINASH/Desktop/html/' + path)
-    path1 = 'c:/users/AVINASH/Desktop/html/'+ path+".png"
+    path1 = 'https://github.com/avianshgaur/cloudSEK/tree/master/1/downloaded/'+ path+".png"
     r = requests.get(url, stream = True, allow_redirects=True)
     with open(path1, 'wb') as f:
         for ch in r:
             f.write(ch)
 
-
+#main download function
 def download(url):
     print('Beginning file download with requests')
     if type(url) is str:
         r = requests.get(url)
         name=filename(url)
-        with open('c:/users/AVINASH/Desktop/html/'+name, 'wb') as f:
+        with open('https://github.com/avianshgaur/cloudSEK/tree/master/1/downloaded'+name, 'wb') as f:
             f.write(r.content)
 
         # Retrieve HTTP meta-data
